dd7992dccb57debdeff84efb6f65467aca763a5a
Uwe Stuehler <ustuehler@growit.io>
2018-02-01 14:49:21 +0100

    refactor: fsInterface now takes a basedir parameter
    This lets us avoid calling process.chdir() during tests, and allows
    the tests to be run concurrently.

--- index.test.js
+++ index.test.js
@@ -1,28 +1,30 @@
 const fs = require('fs')
+const path = require('path')
 const fixtures = require('jest-fixtures')
-const tempDirPath = fixtures.createTempDirSync()
 
-// This should match the Basic Usage example in README.md.
-const monitor = require('./index')({
-  loadConfigFromBucket: false,
-  loadConfigFromSource: false,
-  directories: {
-    source: 'content',
-    target: 'cache'
-  },
-  rules: [{
-    events: ['add','update'],
-    actions: ['copy']
-  },{
-    events: ['delete'],
-    actions: ['deleteAll']
-  }],
-  // This should be the only deviation from the example, which enables
-  // quick local testing without a bucket. Some tests should also be run
-  // against a real bucket, but local testing is all there is for now.
-  dev: true
-})
-
+function createMonitor(tempDirPath) {
+  // This configuration should match the Basic Usage example in README.md.
+  return require('./index')({
+    loadConfigFromBucket: false,
+    loadConfigFromSource: false,
+    directories: {
+      source: 'content',
+      target: 'cache'
+    },
+    rules: [{
+      events: ['add','update'],
+      actions: ['copy']
+    },{
+      events: ['delete'],
+      actions: ['deleteAll']
+    }],
+    // This should be the only deviation from the example, which enables
+    // quick local testing without a bucket. Some tests should also be run
+    // against a real bucket, but local testing is all there is for now.
+    dev: true,
+    basedir: tempDirPath
+  })
+}
 
 describe('Basic Usage example from README.md', () => {
   const gcsAddEvent = {
@@ -42,41 +44,41 @@ describe('Basic Usage example from README.md', () => {
   describe('upload of content/test.jpg', () => {
     it('creates cache/test.jpg', async () => {
       // Test setup
-      await fixtures.copyDir('examples/content', tempDirPath + '/content')
-
-      // TODO: test: avoid process.chdir because it interferes with jest-resolve
-      process.chdir(tempDirPath)
+      const tempDirPath = fixtures.createTempDirSync()
+      const monitor = createMonitor(tempDirPath)
+      await fixtures.copyDir('examples/content', path.join(tempDirPath, 'content'))
 
       // Preconditions and total number of assertions to expect
       expect.assertions(3)
-      expect(fs.existsSync('content/test.jpg')).toBe(true)
-      expect(fs.existsSync('cache/test.jpg')).toBe(false)
+      expect(fs.existsSync(path.join(tempDirPath, 'content/test.jpg'))).toBe(true)
+      expect(fs.existsSync(path.join(tempDirPath, 'cache/test.jpg'))).toBe(false)
 
       // The function's return value is not defined; it's only important
       // that the Promise resolves.
       return monitor(gcsAddEvent).then(() => {
-        expect(fs.existsSync('cache/test.jpg')).toBe(true)
+        expect(fs.existsSync(path.join(tempDirPath, 'cache/test.jpg'))).toBe(true)
       })
     })
   })
 
   describe('upload and removal of content/test.jpg', () => {
-    it.skip('creates and removes cache/test.jpg', async () => {
+    it('creates and removes cache/test.jpg', async () => {
       // Test setup
-      await fixtures.copyDir('examples/content', tempDirPath + '/content')
-      process.chdir(tempDirPath)
+      const tempDirPath = fixtures.createTempDirSync()
+      const monitor = createMonitor(tempDirPath)
+      await fixtures.copyDir('examples/content', path.join(tempDirPath, 'content'))
 
       // Preconditions and total number of assertions to expect
-      expect.assertions(3)
-      expect(fs.existsSync('content/test.jpg')).toBe(true)
-      expect(fs.existsSync('cache/test.jpg')).toBe(false)
+      expect.assertions(4)
+      expect(fs.existsSync(path.join(tempDirPath, 'content/test.jpg'))).toBe(true)
+      expect(fs.existsSync(path.join(tempDirPath, 'cache/test.jpg'))).toBe(false)
 
       // The rules and events are chosen so that a single copy of the source
       // file is created and then removed again in the target directory.
-      monitor(gcsAddEvent).then(() => {
-        expect(fs.existsSync('cache/test.jpg')).toBe(true)
+      return monitor(gcsAddEvent).then(() => {
+        expect(fs.existsSync(path.join(tempDirPath, 'cache/test.jpg'))).toBe(true)
         return monitor(gcsDeleteEvent).then(() => {
-          expect(fs.existsSync('cache/test.jpg')).toBe(false)
+          expect(fs.existsSync(path.join(tempDirPath, 'cache/test.jpg'))).toBe(false)
         })
       })
     })
